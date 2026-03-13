ca28ef1700530125dade693c559666b169f1012a
firthuns <firthunsnpm@gmail.com>
2026-03-13 14:16:57 +0100

    feat: transloco, pages,..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './features/inicio-component/inicio-component';
+import { NosotrosComponent } from './features/nosotros-component/nosotros-component';
+import { TiendaComponent } from './features/tienda-component/tienda-component';
+import { BlogComponent } from './features/blog-component/blog-component';
+import { EntradaComponent } from './features/entrada-component/entrada-component';
+import { GaleriaComponent } from './features/galeria-component/galeria-component';
+import { ContactoComponent } from './features/contacto-component/contacto-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'blog/entrada', component: EntradaComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Footer } from './core/shared/footer/footer';
+import { Header } from './core/shared/header/header';
+import { Navbar } from './core/shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, RouterLink, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('firstPage');
