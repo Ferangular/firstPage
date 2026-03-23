a42689b4c42fe642b5e19934320931729f1258e0
firthuns <firthunsnpm@gmail.com>
2026-03-23 08:27:41 +0100

    feat: imitación udemy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { CursosComponent } from './components/cursos/cursos.component';
-import { DescuentosComponent } from './components/descuentos/descuentos.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, DescuentosComponent, CursosComponent, FooterComponent],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
